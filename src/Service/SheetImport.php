a93feb23d4a2b2edd3be11bb07745cfb63a22d6b
fanomezana livenexx <r.fanomezana@livenexx.net>
2024-02-27 02:41:25 +0300

    handle account and vehicle

--- src/Service/SheetImport.php
+++ src/Service/SheetImport.php
@@ -2,6 +2,18 @@
 
 namespace App\Service;
 
+use App\Entity\Account;
+use App\Entity\Brand;
+use App\Entity\Energy;
+use App\Entity\EventOrigin;
+use App\Entity\Model;
+use App\Entity\Vehicle;
+use App\Repository\AccountRepository;
+use App\Repository\BrandRepository;
+use App\Repository\EnergyRepository;
+use App\Repository\EventOriginRepository;
+use App\Repository\ModelRepository;
+use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
@@ -9,6 +21,12 @@ class SheetImport
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private AccountRepository $accountRepository,
+        private VehicleRepository $vehicleRepository,
+        private BrandRepository $brandRepository,
+        private ModelRepository $modelRepository,
+        private EnergyRepository $energyRepository,
+        private EventOriginRepository $eventOriginRepository,
     )
     {
     }
@@ -19,7 +37,8 @@ class SheetImport
         $data = $spreadsheet->getSheet(0);
         $dataArray = $data->toArray();
 
-        for ($i=1; $i < count($dataArray) ; $i++) { 
+        // for ($i=1; $i < count($dataArray) ; $i++) { 
+        for ($i=1; $i < 2 ; $i++) { 
             $row = $dataArray[$i];
 
             $businessAccount = $row[0];
@@ -59,6 +78,132 @@ class SheetImport
             $eventOrigin = $row[34];
             
         }
+
+        $account = $this->handleAccount($businessAccount);
+        $brand = $this->handleBrand($brandLabel);
+        $model = $this->handleModel($modelLabel, $brand);
+        $energy = $this->handleEnergy($energyLabel);
+        $originEvent = $this->handleEventOrigin($eventOrigin);
+        $eventAccount = $this->handleAccount($eventAccount);
+
+        $vehicle = $this->handleVehicle(
+            $vin,
+            $registrationNo,
+            new \DateTime($dateOfCirculation),
+            $version,
+            $mileAge,
+            $brand,
+            $model,
+            $energy,
+            $originEvent,
+            $eventAccount
+        );
         
     }
-}
+
+    private function saveInstance(object $instance): void{
+        $this->entityManager->persist($instance);
+        $this->entityManager->flush();
+    }
+
+    private function handleAccount(string $accountName): Account
+    {
+        $accountName = trim($accountName);
+        $account = $this->accountRepository->findOneBy(["name" => $accountName]);
+        
+        if(!$account){
+            $account = new Account();
+            $account->setName($accountName);
+            $this->saveInstance($account);
+        }
+        
+        return $account;
+    }
+
+    private function handleVehicle(string $vin, string|null $registrationNo, \Datetime|null $dateOfCirculation, string|null $version, string|null $mileAge, Brand $brand, Model|null $model, Energy|null $energy, EventOrigin $eventOrigin, Account $eventAccount): Vehicle
+    {
+        $vin = trim($vin);
+        $vehicle = $this->vehicleRepository->findOneBy(["vin" => $vin]);
+
+        if(!$vehicle){
+            $vehicle = new Vehicle();
+            $vehicle->setVin($vin);
+            $vehicle->setRegistrationNo($registrationNo);
+            $vehicle->setDateOfCirculation($dateOfCirculation);
+            $vehicle->setVersion($version);
+            $vehicle->setMileAge($mileAge);
+            $vehicle->setBrand($brand);
+            $vehicle->setModel($model);
+            $vehicle->setEnergy($energy);
+            $vehicle->setOriginEvent($eventOrigin);
+            $vehicle->setEventAccount($eventAccount);
+
+            $this->saveInstance($vehicle);
+        }
+
+        return $vehicle;
+    }
+
+    private function handleBrand(string $name): Brand
+    {
+        $name = trim($name);
+        $brand = $this->brandRepository->findOneBy(["name" => $name]);
+
+        if(!$brand){
+            $brand = new Brand();
+            $brand->setName($name);
+    
+            $this->saveInstance($brand);
+        }
+        
+        return $brand;
+    }
+
+    private function handleModel(string $name, Brand $brand): Model
+    {
+        $name = trim($name);
+        $model = $this->modelRepository->findOneBy(["name" => $name, "brand" => $brand]);
+
+        if(!$model)
+        {
+            $model = new Model();
+            $model->setName($name);
+            $model->setBrand($brand);
+    
+            $this->saveInstance($model);
+        }
+        
+        return $model;
+    }
+
+    private function handleEnergy(string $label): Energy
+    {
+        $label = trim($label);
+        $energy = $this->energyRepository->findOneBy(["label" => $label]);
+
+        if(!$energy){
+            $energy = new Energy();
+            $energy->setLabel($label);
+
+            $this->saveInstance($energy);
+        }
+        
+        return $energy;
+    }
+
+    private function handleEventOrigin(string $label): EventOrigin
+    {   
+        $label = trim($label);
+        $eventOrigin = $this->eventOriginRepository->findOneBy(["label" => $label]);
+
+        if(!$eventOrigin)
+        {
+            $eventOrigin = new EventOrigin();
+            $eventOrigin->setLabel($label);
+
+            $this->saveInstance($eventOrigin);
+        }
+        return $eventOrigin;
+    }
+
+}   
